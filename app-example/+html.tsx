e8b29960c0c0610945d0a35c91ee39ea4ed9a219
ceestos <jules.langagne@gmail.com>
2024-07-05 00:12:07 +0200

    Reset of project

--- app-example/+html.tsx
+++ app-example/+html.tsx
@@ -9,6 +9,7 @@ export default function Root({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <head>
+        <title>DnDKaracter</title>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
